bbea85adeda1b8213b71497987db824b4866f959
aryankargwal <kargwalaryan@gmail.com>
2021-03-20 15:13:04 +0530

    search module backend

--- search/search_word.py
+++ search/search_word.py
@@ -1,6 +1,7 @@
-def search_word(df,keywords):
-  for i in df.index:
-    y = df.loc[i, :].values.tolist()
-    y = set(y)
-    if y.intersection(set(keywords)):
-      print(df.iloc[i-1]['time'], df.iloc[i-1]['camera'])
+def search_word(df, keywords):
+    for i in df.index:
+        y = df.loc[i, :].values.tolist()
+        y = set(y)
+        if y.intersection(set(keywords)):
+            # return df.iloc[i - 1]["time"], df.iloc[i - 1]["camera"]
+            return i
