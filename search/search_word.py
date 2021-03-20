a816f7f03ce8cf16dd74d87faa0586bd65deb2a0
indiradutta <66861242+indiradutta@users.noreply.github.com>
2021-03-20 11:44:40 +0530

    update search_word.py

--- search/search_word.py
+++ search/search_word.py
@@ -1,7 +1,8 @@
-def search_word(df, keywords):
-    for i in df.index:
-        y = df.loc[i, :].values.tolist()
-        y = set(y)
-        if y.intersection(set(keywords)):
-            # return df.iloc[i - 1]["time"], df.iloc[i - 1]["camera"]
-            return i
+def search_word(df,keywords):
+  l = []
+  for i in df.index:
+    y = df.loc[i, :].values.tolist()
+    y = set(y)
+    if y.intersection(set(keywords)):
+      l.append(i)
+  return l
